41ea62d2aaced69a014559e9260b233707d0afca
solnyshkoSasha <158512299+solnyshkoSasha@users.noreply.github.com>
2024-05-20 13:00:12 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+// let number = 15;
+// let isHasCar = true;
+
+// if (number > 15 || isHasCar) {
+//   console.log("Yes");
+//   console.log("Yes, he has a car");
+// }
+// else if (number == 15) {
+//   console.log("Num is 15");
+// }
+// else if (number == 10) {
+//   console.log("Num is 10");
+// }
+// else if (number != 1) {
+//   console.log("Num isn`t 1");
+// }
+// else
+//   console.log("Else")
+
+let word = "new2";
+switch (word) {
+  case "new":
+    console.log("Var is new");
+    break;
+  case 5:
+    console.log("Var is 5");
+    break;
+  case "Some":
+    console.log("Var is Some");
+    break;
+  case 15:
+    console.log("Var is new");
+    break;
+  default:
+    console.log("Var is unknown");
+    break;
+}
